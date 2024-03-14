593487328bb9f32470b8d25431ede272032d8359
denisrom88 <53214989+denisrom88@users.noreply.github.com>
2024-03-14 10:06:28 +0200

    Simple "@arcgis/core map

--- app.vue
+++ app.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div id="viewDiv"></div>
 </template>
+
+<script setup lang="ts">
+import Map from "@arcgis/core/Map.js";
+import MapView from "@arcgis/core/views/MapView.js";
+
+onMounted(() => {
+  const map = new Map({
+    basemap: "topo-vector"
+  });
+
+  new MapView({
+    container: "viewDiv",
+    map: map,
+    zoom: 4,
+    center: [15, 65]
+  });
+});
+</script>
+
+<style>
+@import url('https://js.arcgis.com/4.29/@arcgis/core/assets/esri/themes/light/main.css');
+
+html, body, #__nuxt, #viewDiv {
+  height: 100%;
+  margin: 0;
+}
+</style>
